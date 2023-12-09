ce667ef0c407a6a95e11d0e4268031cf7c23b446
E.Y.S.V.S. ABHAY <131472473+abhayemani@users.noreply.github.com>
2023-12-09 13:35:07 +0530

    Add files via upload

---
+++ faculty.js
@@ -0,0 +1,13 @@
+// details.js
+
+// Built-in details
+const builtInDetails = {
+    "name": "John Doe",
+    "email": "john.doe@example.com",
+    "phone": "+1 (123) 456-7890",
+    "university": "Example University"
+};
+
+// Export the details
+export default builtInDetails;
+
---
+++ student.js
@@ -0,0 +1,14 @@
+{
+    students: [
+      {
+        "Name": "John Doe",
+        "Rollno": "22r21a6612",
+        "Gender": "+1 (123) 456-7890",
+        "Branch": "Example University",
+        "CGPA":"8.0",
+        "resume": "john_doe_resume.pdf"
+      }
+
+    ]
+  }
+  
